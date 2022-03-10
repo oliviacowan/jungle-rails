98dabf83f1cf4c6f8dbfc61b4f50adf054b59036
oliviacowan <oliva.cowan@hotmail.com>
2022-03-10 09:36:28 -0800

    Route for admin categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
-  # root to: 'about#index'
   get :about, to: "about#index"
 
   resources :products, only: [:index, :show]
@@ -17,8 +16,9 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories
   end
-
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
