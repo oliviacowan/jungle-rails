c1bc95ec16d027bb866d1c30cd25f2d702cfa9fe
oliviacowan <oliva.cowan@hotmail.com>
2022-03-14 20:26:56 -0700

    switch single name in db to first and last

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,17 +4,18 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to '/'
-    else
-      redirect_to '/signup'
-    end
+      if user.save
+        session[:user_id] = user.id
+        redirect_to '/'
+      else
+        redirect_to '/signup'
+      end
   end
 
   def user_params
     params.require(:user).permit(
-      :name,
+      :first_name,
+      :last_name,
       :email,
       :password,
       :password_confirmation
